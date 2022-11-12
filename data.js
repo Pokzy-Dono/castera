ea15e9b49f498b31177b5d12bcc3a6578892101c
Pokzy-Dono <92625941+Pokzy-Dono@users.noreply.github.com>
2022-11-12 14:27:48 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,702 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entree",
+      "name": "Entree",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1559.5,
+      "initialViewParameters": {
+        "yaw": -0.8587445389940811,
+        "pitch": 0.012756654109280063,
+        "fov": 1.481047542087705
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.8964895792183931,
+          "pitch": -0.050108302642231095,
+          "rotation": 0,
+          "target": "1-salle-a-manger"
+        },
+        {
+          "yaw": -1.8434285231526832,
+          "pitch": 0.1508599045400274,
+          "rotation": 0,
+          "target": "6-terrasse-sud"
+        },
+        {
+          "yaw": 0.5433511629264292,
+          "pitch": 0.03795872842118797,
+          "rotation": 0,
+          "target": "2-garage"
+        },
+        {
+          "yaw": -1.0855124089822183,
+          "pitch": -1.263840274484469,
+          "rotation": 0,
+          "target": "12-vue-aerienne"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.9183136124193982,
+          "pitch": 0.0037004524965915664,
+          "title": "Portail",
+          "text": "Câbles et électricité mise en place pour accueillir des moteurs pour le portail&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "1-salle-a-manger",
+      "name": "Salle a manger",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1791.5,
+      "initialViewParameters": {
+        "yaw": -0.5045591744868378,
+        "pitch": 0.08036646467586372,
+        "fov": 1.481047542087705
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9820352078148744,
+          "pitch": 0.12322966424266113,
+          "rotation": 0,
+          "target": "0-entree"
+        },
+        {
+          "yaw": 0.13140851125277386,
+          "pitch": 0.16443707699011156,
+          "rotation": 0,
+          "target": "6-terrasse-sud"
+        },
+        {
+          "yaw": 0.300768929151463,
+          "pitch": 0.3124574108483493,
+          "rotation": 0,
+          "target": "5-salon"
+        },
+        {
+          "yaw": 1.343577334142239,
+          "pitch": -0.006792147822658023,
+          "rotation": 0,
+          "target": "3-cuisine"
+        },
+        {
+          "yaw": 2.2341645044652845,
+          "pitch": 0.03793397763694273,
+          "rotation": 0,
+          "target": "2-garage"
+        },
+        {
+          "yaw": 1.8790102829189745,
+          "pitch": 0.1353772457888649,
+          "rotation": 0,
+          "target": "4-terrasse-nord"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-garage",
+      "name": "Garage",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1781.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7098249611208516,
+          "pitch": 0.06883444140879114,
+          "rotation": 0,
+          "target": "3-cuisine"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.112564682892092,
+          "pitch": -0.07830299890853354,
+          "title": "Tableau electrique",
+          "text": "Electricité aux normes"
+        }
+      ]
+    },
+    {
+      "id": "3-cuisine",
+      "name": "Cuisine",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1790,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5415174912086052,
+          "pitch": 0.2382624748911244,
+          "rotation": 0,
+          "target": "5-salon"
+        },
+        {
+          "yaw": -0.8693336397008231,
+          "pitch": 0.0034353660348358517,
+          "rotation": 0,
+          "target": "1-salle-a-manger"
+        },
+        {
+          "yaw": -2.030929042190916,
+          "pitch": 0.01711187346507259,
+          "rotation": 0,
+          "target": "2-garage"
+        },
+        {
+          "yaw": -2.96633939066165,
+          "pitch": 0.2934455730894001,
+          "rotation": 0,
+          "target": "4-terrasse-nord"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-terrasse-nord",
+      "name": "Terrasse nord",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1784.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.052634726176814084,
+          "pitch": 0.18120872571488533,
+          "rotation": 0,
+          "target": "3-cuisine"
+        },
+        {
+          "yaw": 0.7580454122999534,
+          "pitch": -1.00660016682151,
+          "rotation": 0,
+          "target": "12-vue-aerienne"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.1919766131773324,
+          "pitch": 0.2359566044097292,
+          "title": "Fosse septique",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-salon",
+      "name": "Salon",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1560.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5024846520576549,
+          "pitch": 0.09629800925205956,
+          "rotation": 0,
+          "target": "0-entree"
+        },
+        {
+          "yaw": -0.08326061181558586,
+          "pitch": 0.24745330229115936,
+          "rotation": 0,
+          "target": "1-salle-a-manger"
+        },
+        {
+          "yaw": -0.9646223176153974,
+          "pitch": 0.06274824278521862,
+          "rotation": 0,
+          "target": "3-cuisine"
+        },
+        {
+          "yaw": -1.4577904809421014,
+          "pitch": 0.0854153382294669,
+          "rotation": 0,
+          "target": "7-couloir"
+        },
+        {
+          "yaw": 2.8736257610342566,
+          "pitch": 0.3292017294318921,
+          "rotation": 0,
+          "target": "6-terrasse-sud"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.7247871430805368,
+          "pitch": -0.3545101487229658,
+          "title": "Conduit pour poele",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "6-terrasse-sud",
+      "name": "Terrasse sud",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1785.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1069373910458395,
+          "pitch": 0.22489824577387907,
+          "rotation": 0,
+          "target": "5-salon"
+        },
+        {
+          "yaw": -0.89700151219877,
+          "pitch": -1.1278318554045264,
+          "rotation": 0,
+          "target": "12-vue-aerienne"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-couloir",
+      "name": "Couloir",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1560,
+      "initialViewParameters": {
+        "yaw": -3.129711133563106,
+        "pitch": 0.15938130569452014,
+        "fov": 1.481047542087705
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0427523687737352,
+          "pitch": 0.2339632410130612,
+          "rotation": 0,
+          "target": "5-salon"
+        },
+        {
+          "yaw": 2.113789748996571,
+          "pitch": 0.29848234744228463,
+          "rotation": 0,
+          "target": "8-salle-de-bain"
+        },
+        {
+          "yaw": 0.9985084703836762,
+          "pitch": 0.477497062312521,
+          "rotation": 0,
+          "target": "11-chambre-3"
+        },
+        {
+          "yaw": -0.4177073398401525,
+          "pitch": 0.000044473422249780015,
+          "rotation": 0,
+          "target": "10-chambre-2"
+        },
+        {
+          "yaw": -0.9864341257393399,
+          "pitch": 0.030328298166068635,
+          "rotation": 0,
+          "target": "9-chambre-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.010983082732874294,
+          "pitch": 0.003563685855869636,
+          "title": "Toilettes",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "8-salle-de-bain",
+      "name": "Salle de bain",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1800,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.094523953357376,
+          "pitch": 0.011501304930527922,
+          "rotation": 0,
+          "target": "9-chambre-1"
+        },
+        {
+          "yaw": -3.129815876114705,
+          "pitch": 0.5486319101159616,
+          "rotation": 0,
+          "target": "7-couloir"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-chambre-1",
+      "name": "Chambre 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1562,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.18370708378037293,
+          "pitch": -0.00928831635276417,
+          "rotation": 0,
+          "target": "11-chambre-3"
+        },
+        {
+          "yaw": 0.18218475236222886,
+          "pitch": 0.43997814457394036,
+          "rotation": 7.853981633974483,
+          "target": "7-couloir"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-chambre-2",
+      "name": "Chambre 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1793,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1469448926903265,
+          "pitch": -0.014017677664519113,
+          "rotation": 0,
+          "target": "11-chambre-3"
+        },
+        {
+          "yaw": 0.15771310089699675,
+          "pitch": 0.3868888176883054,
+          "rotation": 7.0685834705770345,
+          "target": "7-couloir"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-chambre-3",
+      "name": "Chambre 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1564,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.006298304605769189,
+          "pitch": 0.42704375865324806,
+          "rotation": 5.497787143782138,
+          "target": "7-couloir"
+        },
+        {
+          "yaw": 0.10510890776638782,
+          "pitch": 0.04381718255496381,
+          "rotation": 0,
+          "target": "9-chambre-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-vue-aerienne",
+      "name": "Vue aerienne",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.959942587388219,
+          "pitch": 1.5106230875471418,
+          "rotation": 0,
+          "target": "6-terrasse-sud"
+        },
+        {
+          "yaw": -0.4407027608154266,
+          "pitch": 1.427153628068524,
+          "rotation": 1.5707963267948966,
+          "target": "4-terrasse-nord"
+        },
+        {
+          "yaw": 1.0322635932021917,
+          "pitch": 1.4278627789114857,
+          "rotation": 4.71238898038469,
+          "target": "0-entree"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Castera-lou",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
